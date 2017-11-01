343fde5c2196d625eefee0bff049f6f2e2acc9af
worldintro <worldintro@mail.ru>
2017-11-01 12:25:07 +0300

    HE

---
+++ lab5(encrypting)/EncryptingRussianText.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab5_encrypting_
+{
+    class EncryptingRussianText
+    {
+        public struct HelpStruct
+        {
+            public char simbol;
+            public int period; 
+        }
+
+        private string text;
+        private string alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        private string natural = "оеаитнсрвлкмдпуяыгзбчйчъжьюшцщэфё";
+        private HelpStruct[] helpStruct = new HelpStruct[33];
+        public EncryptingRussianText(string text)
+        {
+            this.text = text.ToLower();
+        }
+        public string Encrypting()
+        {
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                for (int j = 0; j < text.Length; j++)
+                {
+                    if (text[j]==alphabet[i])
+                    {
+                        helpStruct[i].simbol = alphabet[i];
+                        helpStruct[i].period++;
+                    }
+                }
+            }
+
+            for(int i = 0; i < helpStruct.Length; i++)
+            {
+                for(int j = 0; j<helpStruct.Length - i - 1; j++)
+                {
+                    if (helpStruct[j].period < helpStruct[j + 1].period)
+                    {
+                        HelpStruct temp = helpStruct[j];
+                        helpStruct[j] = helpStruct[j + 1];
+                        helpStruct[j + 1] = temp;
+                    }
+                }
+            }
+            natural = natural.ToUpper();
+
+            for(int i = 0; i<helpStruct.Length; i++)
+            {
+                text = text.Replace(helpStruct[i].simbol, natural[i]);
+                Console.WriteLine("{0} | {1}", helpStruct[i].simbol, natural[i]);
+            }
+            text = text.ToLower();
+
+            return text;
+        }
+
+    }
+}
---
+++ lab5(encrypting)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lab5_encrypting_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("lab_5.txt", Encoding.Default);
+            String line = sr.ReadToEnd();
+            EncryptingRussianText a = new EncryptingRussianText(line);
+            Console.Write(a.Encrypting());
+            Console.ReadKey();
+        }
+    }
+}
